64999ba8b54b0a11980b1f2f6a44702915a0ff2b
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-07-03 17:47:59 -1000

    secret number

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,13 @@
 'use strict';
 module.exports = function() {
-  
+
+  var secretNumberGenerator = function() {
+    return Math.floor((Math.random()*1000)+1)
+    } 
+    // console.log(secretNumberGenerator());
+    var secretNumber = secretNumberGenerator();
+    var storedNumber = function() {
+        return secretNumber;
+    }
+    return storedNumber;
 };
